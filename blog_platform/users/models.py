c85da4714edfad319e8ed7ce98cf23843e6d5ced
affilpm <affilpm2004@gmail.com>
2025-07-28 21:25:06 +0530

    feat(core, users): add reusable TimeStampedModel and apply to CustomUser

--- blog_platform/users/models.py
+++ blog_platform/users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from core.models.base import  TimeStampedModel
 
 # Create your models here.
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractUser, TimeStampedModel):
     email = models.EmailField(_('email address'), unique=True)
     profile_photo = models.ImageField(upload_to='profile_photos', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
