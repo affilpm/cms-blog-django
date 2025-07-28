1f6d94ede7a4ef49bdb677d70f1fbe003c37497c
affilpm <affilpm2004@gmail.com>
2025-07-28 20:17:24 +0530

    feat(user): add custom user model with email as primary identifier

--- blog_platform/users/models.py
+++ blog_platform/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    profile_photo = models.ImageField(upload_to='profile_photos', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    
+    def __str__(self):
+        return self.email
