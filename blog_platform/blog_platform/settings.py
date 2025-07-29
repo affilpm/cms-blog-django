a281a9729eb761f4675514fa51fd24961795916d
affilpm <affilpm2004@gmail.com>
2025-07-29 13:38:57 +0530

    chore: move DEBUG, SECRET_KEY, ALLOWED_HOSTS to .env; add register template; refactor base.html

--- blog_platform/blog_platform/settings.py
+++ blog_platform/blog_platform/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(b9f=vqrz)^a7r*py8u78+a!t4ekevj-t=9r0ip4_*3ytl+v70'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
