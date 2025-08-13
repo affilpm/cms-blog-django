5e3c111f6bef188cd164ec0bf4befdb1b79c7c67
affilpm <affilpm2004@gmail.com>
2025-08-13 17:05:42 +0530

    feat(admin-comments): add template and JS logic for comment approval toggle

--- blog_platform/admin_panel/urls.py
+++ blog_platform/admin_panel/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
-from .views import DashboardView, UsersListView, UserCreateView, UserEditView, PostListView, AdminPostDetailView
+from .views import DashboardView, UsersListView, UserCreateView, UserEditView, PostListView, AdminPostDetailView, AdminCommentView
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='admin_dashboard'),
-    path('user-list/', UsersListView.as_view(), name='admin_users_list'),
+    # path('', DashboardView.as_view(), name='admin_dashboard'),
+    path('', UsersListView.as_view(), name='admin_users_list'),
     path('user-create/', UserCreateView.as_view(), name='admin_user_create'),
     path('user-edit/<int:user_id>/', UserEditView.as_view(), name='admin_user_edit'),
     
     path('post-list/', PostListView.as_view(), name='admin_post_list'),
     path('post-detail/<int:post_id>/', AdminPostDetailView.as_view(), name='admin_post_detail'),
+    path('comments/', AdminCommentView.as_view(), name='admin_comments')
 ]
 
 
